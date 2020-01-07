f68e3c81f66b47c9c63b03f7d53be6dc8fe4b3f2
Andreas Wiermann Casas <andreas.wiermanncasas@gmail.com>
2020-01-07 09:59:09 -0300

    feat: show movie list with styles

--- components/movie-list/MovieList.js
+++ components/movie-list/MovieList.js
@@ -5,7 +5,26 @@
     for (var i = 0; window.state.movies.length > i; i++) {
       var movie = window.state.movies[i];
 
-      movieList.push(_('p', {}, [movie.title]));
+      var releaseDate = new Date(movie.release_date);
+
+      if (i !== 0) {
+        movieList.push(_('hr', { class: 'movieList-item-divider' }, []));
+      }
+
+      movieList.push(
+        _('div', { class: 'movieList-item' }, [
+          _('img', {
+            class: 'movieList-item-posterImg',
+            src: 'http://image.tmdb.org/t/p/w185/' + movie.poster_path,
+          }),
+          _('div', { class: 'movieList-item-itemText' }, [
+            _('p', { class: 'movieList-item-itemText-title' }, [movie.original_title]),
+            _('p', { class: 'movieList-item-itemText-date' }, [
+              [releaseDate.getDate() + 1, releaseDate.getMonth() + 1, releaseDate.getFullYear()].join('/')
+            ]),
+          ])
+        ])
+      );
     }
 
     return _('div', { class: 'movieList' }, movieList);
